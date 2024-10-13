de9cd7a3fd57d406142aff986b65d7ba1d39063c
DharmilTrivedi <dharmiltrivedi5@gmail.com>
2024-10-13 10:24:43 +0000

    Add post btn updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,33 @@ import { PostComponent } from './post';
 
 function App() {
   // const [count, setCount] = useState(0)
-  const [Profile, setProfile] = useState(PostComponent); //Defining a new state variable
+  const [Post, setPost] = useState([]); //Defining a new state variable
   //When the value of a state variable changes, the component that uses the state variables re-render. 
 
-  function addProfile(){
-    setProfile()
+  const postComponent = Post.map(post => <PostComponent
+    name={post.name}
+    subtitle={post.subtitle}
+    time={post.time}
+    image={post.image}
+    description={post.description}
+  />)
+
+  function addPost() {
+    setPost([...Post, {
+      name: "harkirat",
+      subtitle: "11000 followers",
+      time: "2m ago",
+      image: "https://appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg",
+      description: "What to know how to win big? Check out how these folks won $6000 in bounties."
+    }])
   }
 
   //setIsVisible updates the state
   return <div style={{ background: "#dfe6e9", height: "100vh", }}>
-    <button onClick={addProfile}>Add post</button>
+    <button onClick={addPost}>Add post</button>
     <div style={{ display: "flex", justifyContent: "center" }}>
       <div>
-        <PostComponent
-          name={"harkirat"}
-          subtitle={"11000 followers"}
-          time={"2m ago"}
-          image={"https://appx-wsb-gcp-mcdn.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"}
-          description={"What to know how to win big? Check out how these folks won $6000 in bounties."} />
+        {postComponent}
       </div>
     </div>
   </div>
