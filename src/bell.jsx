580b608123e6c7aaa7b05770863144ef4451beae
DharmilTrivedi <dharmiltrivedi5@gmail.com>
2024-10-13 15:22:54 +0000

    Add tabs and loading function

--- src/bell.jsx
+++ src/bell.jsx
@@ -1,14 +1,41 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export function NotificationBell() {
-    let [notiCount, setNotiCount] = useState(0);
+    const [notiCount, setNotiCount] = useState(0);
+    const [tab, setTab] = useState(1);
+    const [tabData, setTabData] = useState({});
+    const [loading, setLoading] = useState(true);
 
-    function updateNoti() {
-        setNotiCount(notiCount + 1)
-    }
+    useEffect(() => {
+        setInterval(() => {
+            setNotiCount(prev => prev + 1);
+        }, 3000);
+
+        // return () => clearInterval(interval); // Cleanup on unmount
+    }, []);
+
+  
+    useEffect(function() {
+      setLoading(true);
+      fetch("https://jsonplaceholder.typicode.com/todos/" + tab)
+        .then(async res => {
+          const json = await res.json();
+          setTabData(json);
+          setLoading(false);
+        });
+  
+    }, [tab])
+
 
     return (
         <div>
+            <div>
+                <button onClick={()=>setTab(1)} style={{color: (tab == 1) ? "red" : "black"}}>Todo 1</button>
+                <button onClick={()=>setTab(2)} style={{color: (tab == 2) ? "red" : "black"}}>Todo 2</button>
+                <button onClick={()=>setTab(3)} style={{color: (tab == 3) ? "red" : "black"}}>Todo 3</button>
+                <button onClick={()=>setTab(4)} style={{color: (tab == 4) ? "red" : "black"}}>Todo 4</button>
+            </div>
+            {loading ? "Loading....." : tabData.title}
             <div
                 style={{
                     backgroundColor: "red",
@@ -28,8 +55,7 @@ export function NotificationBell() {
                 {notiCount}
             </div>
             <img
-                style={{ width:"50px",height: "50px",cursor: "pointer", marginLeft: 60 }} // Adjust margin to avoid overlap with the notification circle
-                onClick={updateNoti}
+                style={{ width: "50px", height: "50px", cursor: "pointer", marginLeft: 60 }} // Adjust margin to avoid overlap with the notification circle
                 src="https://png.pngtree.com/png-vector/20190411/ourmid/pngtree-vector-bell-icon-png-image_927119.jpg"
                 alt=""
             />
