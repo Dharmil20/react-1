e13877afdf606fbbcb261e59ea921ae39b83554b
DharmilTrivedi <dharmiltrivedi5@gmail.com>
2024-10-23 15:00:47 +0000

    Add clock, start and stop functions

--- useRef/src/App.jsx
+++ useRef/src/App.jsx
@@ -1,19 +1,54 @@
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
 
 function App() {
+  return (
+    <>
+      <Signup />
+      <Clock />
+    </>
+  )
+}
+
+function Clock() {
+  const [currCount, setCurrCount] = useState(0);
+  const timerRef = useRef();
+
+function handleStart() {
+    const interval = setInterval(() => {
+      setCurrCount(prevCount => prevCount + 1);
+    }, 1000);
+
+    timerRef.current = interval;
+  }
+
+  function handleStop(){
+    clearInterval(timerRef.current);
+  }
+
+  return (
+    <div>
+      {currCount}
+      <br />
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
+    </div>
+  );
+}
+
+function Signup() {
   const inputRef = useRef()
 
   const handleFocus = () => {
-    inputRef.focus();
+    inputRef.current.focus();
   }
-  return (
-    <>
-      Sign Up
-      <input ref={inputRef} type="text" />
-      <input type="text" />
-      <button onClick={handleFocus}>Submit</button>
-    </>
+
+  return (<>
+    Sign Up
+    <input ref={inputRef} type="text" />
+    <input type="text" />
+    <button onClick={handleFocus}>Submit</button>
+  </>
   )
 }
 
