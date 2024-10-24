a9eff6c20af475ae52185b562a043770ac660f1f
DharmilTrivedi <dharmiltrivedi5@gmail.com>
2024-10-24 18:15:15 +0000

    Add ContextAPI

--- contextAPI-class/src/App.jsx
+++ contextAPI-class/src/App.jsx
@@ -1,35 +1,43 @@
 import { useState } from 'react'
 import './App.css'
+import { createContext } from 'react';
+import { useContext } from 'react';
+
+const BulbContext = createContext(); //Step 1: Create Context
 
 function App() {
+  const [bulb, setBulb] = useState(true);
   return (
     <>
-    <LightBulb />
+      <BulbContext.Provider value={{ bulb, setBulb }}> {/*Step 2: Provide Context to Parent component */}
+        <LightBulb />
+      </BulbContext.Provider>
     </>
   )
 }
 
-function LightBulb(){
-  const [bulb, setBulb] = useState(true);
+function LightBulb() {
 
-  return(<>
-    <Bulb bulb={bulb}/> <br />
-    <ToggleBulb setBulb={setBulb}/>
+  return (<>
+    <Bulb/> <br />
+    <ToggleBulb />
   </>)
 }
 
-function Bulb({bulb}){
-  return(<>
+function Bulb() {
+  const { bulb } = useContext(BulbContext) //Step 3: Consume The context
+  return (<>
     {bulb ? "Bulb On" : "Bulb Off"}
   </>)
 }
 
-function ToggleBulb({setBulb}){
-  function handleBulb(){
+function ToggleBulb() {
+  const { setBulb } = useContext(BulbContext)
+  function handleBulb() {
     setBulb(currState => !currState);
   }
 
-  return(<>
+  return (<>
     <button onClick={handleBulb}>Toggle</button>
   </>)
 }
